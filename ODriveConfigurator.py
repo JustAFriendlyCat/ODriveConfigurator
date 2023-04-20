15c7a50ba7abc132b679be43e2f5659a53777271
JustAFriendlyCat <41790044+JustAFriendlyCat@users.noreply.github.com>
2023-04-20 21:25:26 +0200

    refactor: idk, changed a bunch of stuff and added a bunch of stuff

--- ODriveConfigurator.py
+++ ODriveConfigurator.py
@@ -1,19 +1,20 @@
 import sys
 import odrive
 import handlers
+import qdarktheme
 
 from PyQt6.QtWidgets import *
 from PyQt6.uic import loadUi
 
 app = QApplication(sys.argv)
+
+qdarktheme.setup_theme("light")
+
 window = QMainWindow()
 loadUi("untitled.ui", window)
 
 odrive.start_discovery(path='usb:idVendor=0x1209,idProduct=0x0D32,bInterfaceClass=0,bInterfaceSubClass=1,bInterfaceProtocol=0')
 
 handler = handlers.GUI_Handler(window)
-
-window.odriveTree.itemClicked.connect(lambda item: handler.odriveTreeHandler(item))
-window.odriveSelect.clicked.connect(lambda: handler.odriveSelectHandler())
 window.show()
 app.exec()
--- handlers.py
+++ handlers.py
@@ -10,26 +10,66 @@ class GUI_Handler:
         self.drive = None
         self.drive_axis = None
         self.currentBox = self.window.generalBox
-
-        #set all boxes to not visible
-        skipFirst = 0
-        for widget in self.window.scrollAreaWidgetContents.children():
-            if skipFirst == 0:
-                skipFirst = 1
-                continue
-            
+        
+        #set all boxes to not visible and skip first element because it is the grid layout
+        for widget in self.window.scrollAreaWidgetContents.children()[1:]:
             widget.setVisible(False)
         else:
             #set generalBox to visible
             self.currentBox.setVisible(True)
         
-        #set all send buttons clicked handler to sendButtonHandler()
-        for button in params.sendButtons:
-            eval(f"self.window.{button}.clicked.connect(self.sendButtonHandler)")
+        #update parameters every second
+        timer = QTimer(self.window)
+        timer.timeout.connect(self.updateParams)
+        timer.start(1000)
+
+        #set odrivetree and odriveselect button to appropriate handlers
+        self.window.odriveTree.itemClicked.connect(lambda item: self.odriveTreeHandler(item))
+        self.window.odriveSelect.clicked.connect(self.odriveSelectHandler)
+
+        #set all textboxes and selectors to setOnEdit(), editingFinished and activated are important because they dont emit signal when value is programmatically changed 
+        for textBox in params.textBoxes:
+            getattr(self.window, textBox).editingFinished.connect(self.setOnEdit)
+        for selector in params.selectors:
+            getattr(self.window, selector).activated.connect(self.setOnEdit)
+
+        #error reporting stuff
+        for textBox in params.textBoxes:
+            if "ErrorText" in textBox:
+                getattr(self.window, textBox).textChanged.connect(self.errorReporting)
 
         #set all controls buttons clicked handler to controlButtonHandler()
         for button in params.controls:
-            eval(f"self.window.{button}.clicked.connect(self.controlButtonHandler)")
+            getattr(self.window, button).clicked.connect(self.controlButtonHandler)
+
+    def errorReporting(self):
+        name = self.window.sender().objectName()
+
+        splitString = params.textBoxes[name].rsplit('.', 1)
+        currentCode = getattr(eval(splitString[0]), splitString[1])
+
+        if currentCode != 0:
+            self.window.debugConsole.appendPlainText(f"[ERROR] Encounted {name[:-9]} error with code: {currentCode}")
+
+    def setOnEdit(self):
+        if self.drive == None: return
+
+        name = self.window.sender().objectName()
+        
+        if "Text" in name:
+            value = getattr(self.window, name).text()
+            s = params.textBoxes[name]
+        elif "Selector" in name:
+            value = getattr(self.window, name).currentIndex()
+            s = params.selectors[name]
+
+        splitString = s.rsplit('.', 1)
+        #['self.drive.can.config', 'baud_rate']
+
+        setattr(eval(splitString[0]), splitString[1], value)
+        #      self.drive.can.config  baud_rate       textbox value  
+
+        self.window.debugConsole.appendPlainText(f"[DEBUG] Set {s} to {value}")
 
     #makes old widget not visible and sets new widget visible
     def setNewBox(self, newBox: str):
@@ -39,94 +79,58 @@ class GUI_Handler:
     
     #handles odrive connection button
     def odriveSelectHandler(self):
-        for item in self.window.odriveList.selectedItems():
-            for drive_ in odrive.connected_devices:
-                if int(item.text()) == drive_.serial_number:
-                    self.drive = drive_
-                    self.odriveTreeHandler(item=self.window.odriveTree.currentItem())
+        if len(self.window.odriveList.selectedItems()) == 0: return
+
+        item = self.window.odriveList.selectedItems()[0]
+
+        for drive in odrive.connected_devices:
+            if int(item.text()) == drive.serial_number:
+                self.drive = drive
+                self.window.debugConsole.appendPlainText(f"[DEBUG] Connected to ODrive {drive.serial_number}")
 
     #handles control buttons
     def controlButtonHandler(self):
-        if self.drive != None:
-            buttonPressedName = self.window.sender().objectName()
-
-            match buttonPressedName:
-                case "reboot":
-                    self.drive.reboot()
-                case "clearErrors":
-                    self.drive.clear_errors()
-                    self.updateParams()
-                case "saveConfiguration":
-                    self.drive.save_configuration()
-                case "eraseConfiguration":
-                    confirmMessageBox = QMessageBox.question(self.window, "Confirm?", "Are you sure you want to erase the configuration?")
-                    if confirmMessageBox == QMessageBox.StandardButton.Yes:
-                        self.drive.erase_configuration()
-                
-    #handles all send buttons
-    def sendButtonHandler(self):
-        buttonPressed = self.window.sender()
-            
-        paramPrefix = buttonPressed.objectName()[:-4]
+        if self.drive != None: return
         
-        if f"{paramPrefix}Text" in params.textBoxes:
-            woa = eval(f"self.window.{paramPrefix}Text").text()
-            s = params.textBoxes[f"{paramPrefix}Text"]
-        #elif f"{paramPrefix}Selector" in params.selectors:
-        else:
-            woa = eval(f"self.window.{paramPrefix}Selector").currentIndex()
-            s = params.selectors[f"{paramPrefix}Selector"]
+        buttonPressedName = self.window.sender().objectName()
 
-        
-        splitString = s.rsplit('.', 1)
-        #['self.drive.can.config', 'baud_rate']
-
-        setattr(eval(splitString[0]), splitString[1], woa)
-        #      self.drive.can.config  baud_rate       textbox value               
-
-        self.updateParams()
-
-        #"encoderModeSelector": "drive_axis.encoder.config.mode"
-        #"requestedStateSelector": "drive_axis.requested_state"
+        match buttonPressedName:
+            case "reboot":
+                self.drive.reboot()
+            case "clearErrors":
+                self.drive.clear_errors()
+                self.window.debugConsole.appendPlainText(f"[DEBUG] Cleared Errors")
+            case "saveConfiguration":
+                self.drive.save_configuration()
+            case "eraseConfiguration":
+                confirmMessageBox = QMessageBox.question(self.window, "Confirm?", "Are you sure you want to erase the configuration?")
+                if confirmMessageBox == QMessageBox.StandardButton.Yes:
+                    self.drive.erase_configuration()
 
     #updates the axis parameters
     def updateParams(self):
-        if self.window.axisSelector.currentIndex() == 0:
-            self.drive_axis = self.drive.axis0
-        elif self.window.axisSelector.currentIndex() == 1:
-            self.drive_axis = self.drive.axis1
+        for device in odrive.connected_devices:
+            if self.window.odriveList.findItems(str(device.serial_number), QtCore.Qt.MatchFlag.MatchContains):
+                continue
+            self.window.odriveList.addItem(str(device.serial_number))
+            self.window.debugConsole.appendPlainText(f"[DEBUG] Found new ODrive {device.serial_number}")
+
+        if self.drive == None: return
+        
+        self.drive_axis = getattr(self.drive, f"axis{self.window.axisSelector.currentIndex()}")
 
         for k, v in params.textBoxes.items():
-            getattr(self.window, k).setText(str(eval(v)))
-            print(v)
+            textBox = getattr(self.window, k)
+            if not textBox.hasFocus():
+                textBox.setText(str(eval(v)))
 
         for k, v in params.selectors.items():
-            if eval(v) == True:
-                getattr(self.window, k).setCurrentIndex(1)
-            else:
-                getattr(self.window, k).setCurrentIndex(0)
+            getattr(self.window, k).setCurrentIndex(int(eval(v)))
 
-        match self.drive_axis.requested_state:
-            #state 5 doesnt exist
-            case 6:
-                self.window.requestedStateSelect.setCurrentIndex(5)
-            case 7:
-                self.window.requestedStateSelect.setCurrentIndex(6)
-            case 8:
-                self.window.requestedStateSelect.setCurrentIndex(7)
-            case 9:
-                self.window.requestedStateSelect.setCurrentIndex(8)
-            case 10:
-                self.window.requestedStateSelect.setCurrentIndex(9)
-            case 11:
-                self.window.requestedStateSelect.setCurrentIndex(10)
-            case 12:
-                self.window.requestedStateSelect.setCurrentIndex(11)
-            case 13:
-                self.window.requestedStateSelect.setCurrentIndex(12)  
-            case _:
-                self.window.requestedStateSelector.setCurrentIndex(self.drive_axis.requested_state)
-        
+        self.window.requestedStateSelector.setCurrentIndex(self.drive_axis.current_state)
+        self.window.inputModeSelector.setCurrentIndex(self.drive_axis.controller.config.input_mode)
+        self.window.controlModeSelector.setCurrentIndex(self.drive_axis.controller.config.control_mode)
+
         match self.drive_axis.encoder.config.mode:
             case 256:
                 self.window.encoderModeSelector.setCurrentIndex(3)
@@ -146,30 +150,27 @@ class GUI_Handler:
         parent = item.parent()
         itemText = item.text(0)
 
-        if self.drive != None and self.window.vbusVoltage.text() == "0.0V":
-            self.updateParams()
-
         match itemText:
             case "General":
                 self.setNewBox("generalBox")
-                for device in odrive.connected_devices:
-                    if self.window.odriveList.findItems(str(device.serial_number), QtCore.Qt.MatchFlag.MatchContains):
-                        continue
-                    self.window.odriveList.addItem(str(device.serial_number))
             case "Axis":
                 self.setNewBox("axisGeneralBox")
             case "Config":
-                match parent.text(0):
-                    case "Axis":   
-                        self.setNewBox("axisConfigBox")
-                    case "Encoder":
-                        self.setNewBox("encoderConfigBox")
-                    case "Controller":
-                        self.setNewBox("controllerConfigBox")
-                    case "Can":
-                        self.setNewBox("canConfigBox")
-                    case "Motor":
-                        self.setNewBox("motorConfigBox")
+                if parent != None:
+                    match parent.text(0):
+                        case "Axis":   
+                            self.setNewBox("axisConfigBox")
+                        case "Encoder":
+                            self.setNewBox("encoderConfigBox")
+                        case "Controller":
+                            self.setNewBox("controllerConfigBox")
+                        case "Can":
+                            self.setNewBox("canConfigBox")
+                        case "Motor":
+                            self.setNewBox("motorConfigBox")
+                else:
+                    self.setNewBox("config")
+                
             case "General Lockin":
                 self.setNewBox("generalLockinBox")
             case "Sensorless Ramp":
